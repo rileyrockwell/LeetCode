46e915ff8af73d8446a2141cb9ef5b75a916deed
Riley Rockwell <rileyrockwell@gmail.com>
2023-03-11 19:29:36 -0800

    update

---
+++ Competitions/336.2.py
@@ -0,0 +1,61 @@
+# 6316. Rearrange Array to Maximize Prefix Score
+'''
+nums = [2,-1,0,1,-3,3,-3]
+
+objective: rearrange nums in whatever order the user determines: nums = [...]
+
+constraints:
+1 <= nums.length <= 105
+-106 <= nums[i] <= 106
+'''
+
+
+def prefix(nums, i):
+    '''
+    nums: array of ints
+    i: int
+
+    return: sum of the first i-th elements in the nums
+    '''
+    counter = 0
+    for integer in nums[:i + 1]:
+        if integer > 0:
+            counter += integer
+    return counter
+
+
+nums = [2,-1,0,1,-3,3,-3]
+
+def rearrange(nums):
+    new_list = []
+    for i in nums:
+        if i > 0:
+            new_list.append(i)
+    return new_list
+
+print(rearrange(nums))
+
+def prefix(nums):
+    # i-th element is the sume of the (i + (i- 1))-th element
+    new_list = []
+    for i in range(len(nums)):
+        if i == 0:
+            new_list.append(nums[i])
+        elif i > 0:
+            new_list.append(nums[i] + nums[i - 1])
+    return new_list
+
+def maximum_element(nums):
+    return max(nums)
+
+
+print(max(prefix(rearrange(nums))))
+
+
+def sum_of_nums(nums):
+    total_sum = 0
+    for i in nums:
+        total_sum += i
+    return total_sum
+
+print(sum_of_nums(nums))
--- Exercises/ToComplete/1TwoSum.py
+++ Exercises/ToComplete/1TwoSum.py
@@ -1,6 +1,14 @@
 class Solution:
-    def twoSum(self, nums: int, target: int) -> int:
-        pass
+    def twoSum(self, nums: [int], target: int) -> [int]:
+        nums_copy = nums
+        list_without_ith = []
+        result = []
+        for i in nums_copy:
+            # for j in nums_copy.remove from the i-th index from nums_copy
+            if i + j == target:
+                result.append(i)
+                result.append(j)
+                # result.append(i, j)
 
 if __name__ == "__main__":
     print(Solution().twoSum([0, 1, 2, 3, 4, 5, 6], 10))
